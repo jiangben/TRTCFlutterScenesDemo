9ad8d2e0c9672373ecc5b0470fdf82f11467e930
abenzone <47379449@qq.com>
2021-07-21 14:15:55 +0800

    key and id

--- lib/debug/GenerateTestUserSig.dart
+++ lib/debug/GenerateTestUserSig.dart
@@ -26,7 +26,7 @@ class GenerateTestUserSig {
    * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/trtc ) 创建应用，即可看到 SDKAppId，
    * 它是腾讯云用于区分客户的唯一标识。
    */
-  static int sdkAppId = 0;
+  static int sdkAppId = 1400535032;
 
   /*
    * 签名过期时间，建议不要设置的过短
@@ -46,7 +46,8 @@ class GenerateTestUserSig {
    * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
    * 文档：https://cloud.tencent.com/document/product/647/17275#Server
    */
-  static String secretKey = '';
+  static String secretKey =
+      '4987006696c8098833687738b133a00e1bca58660bd9986443ee22031c207f36';
 
   ///生成UserSig
   static genTestSig(String userId) {
